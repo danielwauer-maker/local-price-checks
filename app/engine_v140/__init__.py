c4863e345044582262188f86a760e23f11d20d82
danielwauer-maker <daniel.wauer@gmail.com>
2026-08-16 20:02:15 +0200

    feat: add isolated v1.4 extractor namespace

---
+++ app/engine_v140/__init__.py
@@ -0,0 +1,6 @@
+"""Benchmarked Local Price Checks 1.4.0 extraction engine.
+
+This namespace is intentionally isolated from the mobile Web-MVP data model while
+we migrate the proven REWE/Netto/ALDI parser pipeline.  Adapters in the Web app
+consume its CollectedOffer output and map it to MasterProduct/Offer records.
+"""
