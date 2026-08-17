09dd7a2768af50a2a558a758ebbf40fb6a91f7d2
danielwauer-maker <daniel.wauer@gmail.com>
2026-08-17 20:29:49 +0200

    feat: mount frontend UX endpoints

--- app/api_main.py
+++ app/api_main.py
@@ -2,12 +2,14 @@ from .main import app
 from .api_routes import router
 from .admin_routes import router as admin_router
 from .media_routes import router as media_router
+from .ux_routes import router as ux_router
 from .admin_seed import seed_admin_catalog
 from .db import SessionLocal
 
 app.include_router(router)
 app.include_router(admin_router)
 app.include_router(media_router)
+app.include_router(ux_router)
 
 
 @app.on_event("startup")
