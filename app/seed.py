7f9d91467753078d74b9b1fd0ef42710c4f9d432
danielwauer-maker <daniel.wauer@gmail.com>
2026-08-17 13:13:33 +0200

    Store official Netto prospect IDs

--- app/seed.py
+++ app/seed.py
@@ -5,8 +5,8 @@ from .models import Store
 STORES = [
     ("REWE", "REWE:XL Hundertmark", "56269", "Dierdorf", "Königsberger Str. 20-22", 50.5474, 7.6506, True, "321019"),
     ("REWE", "REWE Dennis Weirich", "56587", "Straßenhaus", "Kirschbüchel 2", 50.5407, 7.5187, False, "1940425"),
-    ("Netto Marken-Discount", "Netto Dierdorf", "56269", "Dierdorf", "Königsberger Str. 24", 50.5472, 7.6510, True, None),
-    ("Netto Marken-Discount", "Netto Oberhonnefeld-Gierend", "56587", "Oberhonnefeld-Gierend", "Über dem Stellweg 25", 50.5565, 7.5154, True, None),
+    ("Netto Marken-Discount", "Netto Dierdorf", "56269", "Dierdorf", "Königsberger Str. 24", 50.5472, 7.6510, True, "6822"),
+    ("Netto Marken-Discount", "Netto Oberhonnefeld-Gierend", "56587", "Oberhonnefeld-Gierend", "Über dem Stellweg 25", 50.5565, 7.5154, True, "2648"),
     ("ALDI SÜD", "ALDI SÜD Dierdorf", "56269", "Dierdorf", "Königsberger Str. 50", 50.5490, 7.6558, True, None),
     ("ALDI SÜD", "ALDI SÜD Oberhonnefeld-Gierend", "56587", "Oberhonnefeld-Gierend", "Über dem Stellweg 5", 50.5550, 7.5200, True, None),
     ("EDEKA", "EDEKA Fellenzer", "56305", "Puderbach", "Urbacher Str. 35", 50.6000, 7.6110, False, "071378"),
