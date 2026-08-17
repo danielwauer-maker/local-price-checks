a1cabf30cd036a531312b540e247e7bccc58b7ea
danielwauer-maker <daniel.wauer@gmail.com>
2026-08-17 19:43:17 +0200

    feat: expose uploaded media through backend

--- app/api_main.py
+++ app/api_main.py
@@ -1,6 +1,8 @@
 from .main import app
 from .api_routes import router
 from .admin_routes import router as admin_router
+from .media_routes import router as media_router
 
 app.include_router(router)
 app.include_router(admin_router)
+app.include_router(media_router)
