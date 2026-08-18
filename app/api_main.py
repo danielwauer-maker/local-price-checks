bb54ce5e94cc0d313acd3daf21d04fd86bad4b50
danielwauer-maker <daniel.wauer@gmail.com>
2026-08-18 17:45:28 +0200

    Register coverage and onboarding routes

--- app/api_main.py
+++ app/api_main.py
@@ -4,6 +4,9 @@ from .admin_routes import router as admin_router
 from .admin_collector_routes import router as admin_collector_router
 from .admin_provenance_routes import router as admin_provenance_router
 from .admin_prospect_audit_routes import router as admin_prospect_audit_router
+from .admin_coverage_routes import router as admin_coverage_router
+from .coverage_routes import router as coverage_router
+from .coverage_models import CoverageRegion  # noqa: F401 - registers additive table before startup create_all
 from .media_routes import router as media_router
 from .ux_routes import router as ux_router
 from .prospect_routes import router as prospect_router
@@ -16,6 +19,8 @@ app.include_router(admin_router)
 app.include_router(admin_collector_router)
 app.include_router(admin_provenance_router)
 app.include_router(admin_prospect_audit_router)
+app.include_router(admin_coverage_router)
+app.include_router(coverage_router)
 app.include_router(media_router)
 app.include_router(ux_router)
 app.include_router(prospect_router)
