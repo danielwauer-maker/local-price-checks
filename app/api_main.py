e0dd9f5cfb1e059350c1775848b6473dd8742b7a
danielwauer-maker <daniel.wauer@gmail.com>
2026-08-18 19:48:20 +0200

    Register admin media delete routes

--- app/api_main.py
+++ app/api_main.py
@@ -7,6 +7,7 @@ from .client_routes import router as client_router
 from .admin_routes import router as admin_router
 from .admin_users_routes import router as admin_users_router
 from .admin_data_status_routes import router as admin_data_status_router
+from .admin_media_routes import router as admin_media_router
 from .admin_collector_routes import router as admin_collector_router
 from .admin_provenance_routes import router as admin_provenance_router
 from .admin_prospect_audit_routes import router as admin_prospect_audit_router
@@ -54,6 +55,7 @@ app.include_router(client_router)
 app.include_router(admin_router)
 app.include_router(admin_users_router)
 app.include_router(admin_data_status_router)
+app.include_router(admin_media_router)
 app.include_router(admin_collector_router)
 app.include_router(admin_provenance_router)
 app.include_router(admin_prospect_audit_router)
