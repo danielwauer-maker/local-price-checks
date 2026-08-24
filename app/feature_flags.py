31636503de049c312c7be86dea28cc271d0d22d8
danielwauer-maker <daniel.wauer@gmail.com>
2026-08-24 13:53:18 +0200

    Keep unbacked product alternatives disabled in pilot

--- app/feature_flags.py
+++ app/feature_flags.py
@@ -16,7 +16,7 @@ DEFAULT_FEATURE_FLAGS: dict[str, bool] = {
     "savings": False,
     "normal_price_badges": False,
     "price_alerts": False,
-    "product_alternatives": True,
+    "product_alternatives": False,
     "reviewer_mode": True,
 }
 
