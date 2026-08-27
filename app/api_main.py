2a1643382f3fc99ea8f7b0abe0dc1ca838e603ba
danielwauer-maker <daniel.wauer@gmail.com>
2026-08-27 21:15:32 +0200

    Register shopping and favorite sharing routes

--- app/api_main.py
+++ app/api_main.py
@@ -21,6 +21,8 @@ from .lokero_routes import router as lokero_router
 from .lokero_state_routes import router as lokero_state_router
 from .lokero_admin_routes import router as lokero_admin_router
 from .lokero_media_routes import router as lokero_media_router
+from .shopping_sharing_routes import router as shopping_sharing_router
+from .favorite_sharing_routes import router as favorite_sharing_router
 from . import model_registry as model_registry  # noqa: F401 - registers the complete schema
 from .coverage_models import CoverageRegion  # noqa: F401 - imported for existing route references
 from .client_models import (  # noqa: F401 - registers additive tables before startup create_all
@@ -115,6 +117,8 @@ app.include_router(lokero_router)
 app.include_router(lokero_state_router)
 app.include_router(lokero_admin_router)
 app.include_router(lokero_media_router)
+app.include_router(shopping_sharing_router)
+app.include_router(favorite_sharing_router)
 app.include_router(media_router)
 app.include_router(ux_router)
 app.include_router(prospect_router)
